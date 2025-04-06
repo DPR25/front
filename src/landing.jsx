c75683009f9d384a5cd44de1c9636b6ea50f2e8a
r4inmaker <jakobsircelj@gmail.com>
2025-04-06 23:33:21 +0200

    landing 1.0

---
+++ src/landing.jsx
@@ -0,0 +1,68 @@
+import {React} from 'react';
+import './landing.css'
+
+function Landing() {
+    return <>
+    <div className='landing relative flex justify-center w-full h-dvh gap-4 p-5'>
+        <div className='absolute top-[20vh]'>
+            <div className=' flex justify-center gap-3 py-6'>
+                <img src="./satelite.svg" alt="" width={42}/>
+                <h1 className='text text-5xl'>Timber AI </h1>
+            </div>
+
+            <div className='typewriter flex font-light text-xl justify-center'>
+                <h2>Digital twins for forest preservation.</h2>
+            </div>
+        </div>
+
+        <div className='absolute top-[40vh] w-[50vw] min-w-[800px] h-auto flex justify-center items-center gap-4'>
+            <div className='flex flex-col gap-4'>
+                <div className='glass relative w-[400px] h-[28vh] slide-animate0 overflow-hidden'>
+                    <div className='flex flex-col items-center'>
+                        <h1 className='absolute top-3 left-40 text-gray-200 text-2xl font-semibold'>Sentinel-2 Images</h1>
+                        <p className='absolute top-15 left-20 mr-2.5 text-right text-'>
+                            Powered by Copernicus API we use latest Sentinel-2 
+                            data to keep up with
+                            <br /> rapid changes in most 
+                            <br />critical regions of 
+                            <br />the world.
+                            </p>
+                        <img src="./sentinel-2.png" alt="" className='absolute top-10 right-5 w-full'/>
+                    </div>
+                </div>
+
+                <div className='glass relative w-[400px] h-[28vh] slide-animate2 overflow-hidden'>
+                    <img src="./ai-brain.png" alt="" 
+                    className='absolute bottom-25 right-18'/>
+                    <h1 className='absolute top-30 left-5 text-gray-200 text-2xl font-semibold'>Cutting Edge AI</h1>
+                    <p className='absolute top-40 left-5 text-gray-300'>
+                        Using the latest state-of-the-art <br />
+                        deep learning models, we create and store <br />
+                        timestamped digital twins to be used for detecting forest landmass changes in real time.
+                    </p>
+                </div>
+            </div>
+
+            <div className='glass relative w-[400px] h-[57.5vh] slide-animate1 overflow-hidden'>
+                <div className='glass'>
+                <h1 className='absolute top-10 left-22  text-gray-200 text-3xl font-semibold'>Interpretable Visualizations</h1>
+                <p className='absolute top-40 left-5 text-gray-300 font-semibold'> 
+                    </p>
+                </div>
+                <div className="absolute top-35 flex flex-col justify-center items-center w-full gap-4"> 
+                    <img src="./deforestation-img.png" 
+                    className=" rounded-2xl w-[86%]"
+                    alt="" />
+                    <button className="w-[100px] h-[45px] hover:cursor-pointer border- text-xl 
+                    font-light rounded-[10px] hover:text-gray-500 transition-all"> Sign In
+                    </button>
+                </div>
+
+            </div>  
+        </div>
+        
+    </div>
+    </>
+}
+
+export default Landing
